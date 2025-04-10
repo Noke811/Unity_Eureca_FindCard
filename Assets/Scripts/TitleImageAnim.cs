af2ace661017119f4e38c86c5a3dafd2a1d65088
Noke <k3357943@gmail.com>
2025-04-10 17:43:31 +0900

    modify font&background

--- Assets/Scripts/TitleImageAnim.cs
+++ Assets/Scripts/TitleImageAnim.cs
@@ -8,6 +8,8 @@ public class TitleImageAnim : MonoBehaviour
 
     public float period;
     float timer = 0f;
+    int save = -1;
+    int temp;
 
     private void Awake()
     {
@@ -21,7 +23,14 @@ public class TitleImageAnim : MonoBehaviour
         if(timer >= period)
         {
             timer = 0f;
-            spriter.sprite = photos[Random.Range(0, photos.Length)];
+
+            temp = Random.Range(0, photos.Length);
+            if (temp == save)
+                save = (temp == 0 ? photos.Length : temp) - 1;
+            else
+                save = temp;
+
+            spriter.sprite = photos[save];
         }
     }
 }
